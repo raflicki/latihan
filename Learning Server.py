94ba2a861d44039cdab8ab19c5cce948d850a42f
rafli licki <rafli.husein10@gmail.com>
2022-11-07 10:05:45 +0700

    latihan

---
+++ Learning Server.py
@@ -0,0 +1,17 @@
+from unittest import result
+
+
+rafli = "ganteng"
+print (rafli)
+
+var1 = 90
+var2 = 40
+var3 = var1 / var2
+print (var3)
+
+var5 = "someone"
+print (var5)
+
+orang = "apalah ini"
+print(orang[2::3])
+
---
+++ latihan.py
@@ -0,0 +1,28 @@
+##Soal 1
+soal = "Hello World to python"
+print (soal)
+
+##Soal 2
+var = 10
+var1 = 20
+var2 = var + var1
+var3 = var1 - var
+var4 = var1 * var
+var5 = var1 / var
+print (var2)
+print (var3)
+print (var4)
+print (var5)
+
+##Soal 3
+a = 10
+b = 20
+c = a / b
+d = a // b
+print (c)
+print (d)
+
+#Soal 4
+usia = 18
+rafli = "usiaku:"
+print (rafli,usia)
